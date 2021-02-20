8f49fef818c88cf7c45761105cba243cc695a9e2
taylorschmidt <taylorhillschmidt@gmail.com>
2021-02-20 15:30:11 -0700

    updated url typo

--- components/Register.js
+++ components/Register.js
@@ -31,7 +31,7 @@ const SignUpForm = () => {
     const register = (e) => {
         e.preventDefault()
         axios.post(
-            'http://localhost:8000' + `/api/v1/user/register`,
+            process.env.REACT_APP_BACKEND_URL + `/api/v1/user/register`,
             {username: username,
             email: email,
             password: password
